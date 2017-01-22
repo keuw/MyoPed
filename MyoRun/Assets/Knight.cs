528b30d3fcea4994d0f47146a7914600a610c1b7
Justin Duong <justin.duong53@gmail.com>
2017-01-22 02:25:06 -0500

    Finished Lever Event Functionality

---
+++ MyoRun/Assets/Knight.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knight : MonoBehaviour {
+    private Animator playerAnimator;
+    public Transform playerTrans;
+    public bool atGoal;
+    public bool winMove;
+
+    public float enterWait = 3;
+
+    // Use this for initialization
+    void Start () {
+        playerAnimator = GetComponent<Animator>();
+        playerTrans = GetComponent<Transform>();
+
+        atGoal = false;
+        winMove = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (playerTrans.position.x <= -1) {
+            playerAnimator.SetInteger("AnimationState", 1);
+            playerTrans.Translate(-Time.deltaTime*3, 0, 0);
+        }
+        else
+        {
+            
+            if (winMove == false)
+            {
+                atGoal = true;
+                playerAnimator.SetInteger("AnimationState", 2);
+                
+            }
+        }
+        
+        if (winMove == true)
+        {
+            if (enterWait > 0)
+            {
+                enterWait -= Time.deltaTime;
+                playerAnimator.SetInteger("AnimationState", 0);
+            }
+            else
+            {
+                playerTrans.Translate(-Time.deltaTime * 3, 0, 0);
+                playerAnimator.SetInteger("AnimationState", 1);
+            }
+        }
+
+    }
+}
--- MyoRun/Assets/Scripts/GameHandler.cs
+++ MyoRun/Assets/Scripts/GameHandler.cs
@@ -5,34 +5,59 @@ using UnityEngine;
 public class GameHandler : MonoBehaviour {
     private float verticalSize;
     private float horizontalSize;
+
     public GroundBlock block;
+    public Knight knight;
+    public Lever lever;
+    public Bridge bridge;
+    private Animator bridgeAnim;
 
-    private double mapX = 100.0;
-    private double mapY = 100.0;
 
-    private float minX;
-    private float maxX;
-    private float minY;
-    private float maxY;
+    public bool win;
 
-	// Use this for initialization
-	void Start () {
+    public int animationState = 0;
+
+
+    public int eventSet = 0;
+    // Use this for initialization
+    void Start () {
+        win = false;
+
         verticalSize = Camera.main.orthographicSize;
         horizontalSize = verticalSize * Screen.width / Screen.height;
 
-        minX = horizontalSize - (float)mapX / 2.0f;
-        maxX = (float)mapX / 2.0f - horizontalSize;
-        minY = verticalSize - (float)mapY / 2.0f;
-        maxY = (float)mapY / 2.0f - verticalSize;
+        bridgeAnim = bridge.GetComponent<Animator>();
 
-        for (int i = 0; i < 15; i++)
+        for (int i = 0; i < 20; i++)
         {
-            Instantiate(block, new Vector3((i * 1f)-horizontalSize, 0- verticalSize+0.95f, 0),Quaternion.identity);
+            Instantiate(block, new Vector3((i * 0.5f)-horizontalSize, 0- verticalSize+0.48f, 0),Quaternion.identity);
         }
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Space) && knight.atGoal == true)
+        {
+            lever.GetComponent<SpriteRenderer>().color = new Color(0, 0, 255);
+            knight.winMove = true;
+            
+        }
+        if (knight.winMove == true)
+        {
+            if (knight.enterWait > 0)
+            {
+                bridgeAnim.Play("Bridge_drop");
+
+            }
+        }
+        if (knight.playerTrans.position.x >= 10)
+        {
+            win = true;
+        }
+    }
+
 }
